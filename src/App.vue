f41b09abec763f9d04ef81b506394b0b7d044c3e
renfangfeng <2363618406@qq.com>
2021-03-30 11:01:21 +0800

    项目初始化完成

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
 <div id="app">
     <h1>黑马</h1>
     <router-view></router-view>
+    <i class="iconfont icon-fenxiang"></i>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
 </div>
 </template>
 
